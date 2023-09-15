12dab25ffa38d88891f7406c225c5862c8e60cfe
Lucas Henriques <lucashenriques@localhost.localdomain>
2023-09-14 21:28:53 -0300

    Initial commit

---
+++ app/layout.tsx
@@ -0,0 +1,44 @@
+import Link from "next/link"
+import "./globals.css"
+import { Inter } from "next/font/google"
+import { ThemeProvider } from "@/components/theme-provider"
+import { Analytics } from "@/components/analytics"
+import { ModeToggle } from "@/components/mode-toggle"
+
+const inter = Inter({ subsets: ["latin"] })
+
+export const metadata = {
+  title: "Lucas Henriques",
+  description: "Desenvolvedor de software e negócios por aí",
+}
+
+interface RootLayoutProps {
+  children: React.ReactNode
+}
+
+export default function RootLayout({ children }: RootLayoutProps) {
+  return (
+    <html lang="en">
+      <body
+        className={`antialiased min-h-screen bg-white dark:bg-slate-950 text-slate-900 dark:text-slate-50 ${inter.className}`}
+      >
+        <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
+          <div className="max-w-2xl mx-auto py-10 px-4">
+            <header>
+              <div className="flex items-center justify-between">
+                  <Link href="/">henriques.</Link>
+                <nav className="ml-auto text-sm font-medium space-x-6 flex items-center">
+                  <Link href="/sobre">sobre</Link>
+                  <Link href="/projeto-henri">projeto henri</Link>
+                  <ModeToggle />
+                </nav>
+              </div>
+            </header>
+            <main>{children}</main>
+          </div>
+          <Analytics />
+        </ThemeProvider>
+      </body>
+    </html>
+  )
+}
