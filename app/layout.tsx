3fe40b3740a75cdb765a0ab4c86af63da5b62ba1
Lucas Henriques <lucashenriques@localhost.localdomain>
2024-01-01 15:17:22 -0300

    Translation of my first post and new layout updates

--- app/layout.tsx
+++ app/layout.tsx
@@ -42,6 +42,11 @@ export default function RootLayout({ children }: RootLayoutProps) {
                 <nav className="ml-auto text-sm font-medium space-x-6 flex items-center">
                   <Link href="/about">about</Link>
                   <Link href="/portfolio">portfolio</Link>
+                  <a target="_blank" href="https://github.com/henriquesss">
+                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="24" height="24" fill="currentColor">
+                      <path d="M12 0a12 12 0 0 0-3.792 23.404c.6.12.792-.252.792-.576v-2.004c-3.252.708-3.936-1.536-3.936-1.536-.48-1.176-1.176-1.488-1.176-1.488-.96-.648.072-.636.072-.636 1.068.072 1.632 1.116 1.632 1.116.96 1.704 2.52 1.2 3.132.912.096-.696.36-1.2.648-1.464-2.256-.252-4.608-1.128-4.608-5.04 0-1.116.408-2.028 1.08-2.748-.108-.252-.468-1.296.108-2.7 0 0 .864-.276 2.82 1.044a9.502 9.502 0 0 1 2.496-.336 9.51 9.51 0 0 1 2.496.336c1.956-1.32 2.82-1.044 2.82-1.044.576 1.404.216 2.448.108 2.7.672.72 1.08 1.632 1.08 2.748 0 3.912-2.352 4.788-4.584 5.04.36.324.672.936.672 1.896v2.82c0 .324.192.708.792.576A12 12 0 0 0 12 0z" />
+                    </svg>
+                  </a>
                   <ModeToggle />
                 </nav>
               </div>
